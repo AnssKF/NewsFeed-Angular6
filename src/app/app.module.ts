6367bbcf19074419da836761806c509a94d0eed6
anssk <anss.khaled00@gmail.com>
2019-06-05 21:04:40 +0200

    Create main view component
    -- Default rout point to main component
    -- Add Global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './Components/main/main.component';
+import { NewsFeedComponent } from './Components/news-feed/news-feed.component';
+import { PostComponent } from './Components/news-feed/post/post.component';
+import { NewsFeedService } from './Services/news-feed.service';
+import { LeftSidebarComponent } from './Components/left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './Components/right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NewsFeedComponent,
+    PostComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    NewsFeedService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
